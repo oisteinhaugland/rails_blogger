2cf7a5c45c0a4ecbf4ff763bee61b106bf3f4e1c
Øistein Haugland <haugland.1193@gmail.com>
2017-02-02 18:18:20 +0100

    added a few gems

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,8 +19,9 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		@article.save
 
-		flash.notice = "Article '#{@article.title}' Created!"
-  		redirect_to article_path(@article)
+		flash.notice = "Article '#{@article.title}' Created"
+
+		redirect_to article_path(@article)
 	end
 
 	def edit
